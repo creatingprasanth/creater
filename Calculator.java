4bf777cf6c361f2758488e610abe27d5181a8ea6
creatingprasanth.com <prasanth24998@gmail.com>
2021-12-27 13:37:57 +0530

    Done

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator
+{
+public static void main(String[] args)
+{
+Calculator calc = new Calculator();
+float output=calc.add();
+add()
+System.out.println(output);
+}
+public float add()
+{
+	int no1=10; 
+	float no2=20;
+	float result =no1 + no2;
+	return result;
+}
+}
